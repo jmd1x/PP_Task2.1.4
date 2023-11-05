961584fc1aff345f59fb2569ab179e41e177d83c
jmd1x <144138299+jmd1x@users.noreply.github.com>
2023-11-05 16:06:55 +0300

    Version 1.0

--- src/main/java/app/model/Dog.java
+++ src/main/java/app/model/Dog.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Component;
 
 @Component("dogBean")
 public class Dog extends Animal{
-    public Dog(){
-        System.out.println("Создан бин собаки");
-    }
+
     @Override
     public String toString() {
         return "I'm a Dog";
